940e4e541c350b2e8abfd21ca50dcf0f90e636b3
ji sun lee <ellijs@hotmail.com>
2021-03-17 21:01:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
+
+
+require_relative './spec/fizzbuzz_spec.rb'
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elseif int % 5 == 0
+    return "Buzz"
+  elseif int % 15 == 0
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
+
+fizzbuzz(10)
