0815b85dba21085a40749c7995aebc88266fd203
ji sun lee <ellijs@hotmail.com>
2021-03-17 21:38:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
 
-  if int % 5 == 0
+  elseif int % 5 == 0
     return "Buzz"
 
-  if int % 15 == 0
+  elseif int % 15 == 0
     return "FizzBuzz"
 
   else
